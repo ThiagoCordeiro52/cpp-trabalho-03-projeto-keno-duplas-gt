ad77f2556c023f89ae71ca132b817cd73fb95e11
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-07-06 21:24:32 -0300

    version with partial data read

--- source/core/main.cpp
+++ source/core/main.cpp
@@ -10,9 +10,41 @@
 
 #include "keno_common.h"
 
+// int strToInt ( std::string inputStr_ ) {
+// // Criando o fluxo ‘ iss ’ e associando - o a ‘ inputStr_ ’.
+//     std::istringstream iss ( inputStr_ );
+//     int value;
+//     iss >> value >> std::ws ; // Ignora espaço em branco depois .
+//     if ( iss.fail() or !iss.eof()) {
+//         std::cerr << " strToInt (): Erro , illegal integer format .\n";
+//     }
+//     return value;
+// }
+
 int main( int argc, char *argv[] )
 {
-    // TODO
+    if (argc < 2) {
+        std::cout << "Please give a file as an argument.\n";
+        return 1;
+    }
+    int counter {0};
+    std::string val;
+    std::string arquivo {"../../data/"};
+    arquivo += argv[1];
+    std::ifstream ifs (arquivo); // 2 em 1: Abre o " arquivo . txt " E associa ao fluxo ‘ ifs ’.
+    while ( ifs >> val ) {
+        std::cout << val << "\n";
+        ++counter;
+        if((counter == 1 || counter == 2) && val.empty())
+        {
+            std::cout << "ERROR";
+            break;
+
+        }
+    }
+    ifs.close();
+    std::cout << "Total" << counter << "\n";
+
 
     return EXIT_SUCCESS;
 }
