ddeed015cfbc6576277d6970499a66f5e1b03ad4
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-07-09 19:13:44 -0300

    version with partial data read changed

--- source/core/main.cpp
+++ source/core/main.cpp
@@ -10,40 +10,37 @@
 
 #include "keno_common.h"
 
-// int strToInt ( std::string inputStr_ ) {
-// // Criando o fluxo ‘ iss ’ e associando - o a ‘ inputStr_ ’.
-//     std::istringstream iss ( inputStr_ );
-//     int value;
-//     iss >> value >> std::ws ; // Ignora espaço em branco depois .
-//     if ( iss.fail() or !iss.eof()) {
-//         std::cerr << " strToInt (): Erro , illegal integer format .\n";
-//     }
-//     return value;
-// }
-
 int main( int argc, char *argv[] )
 {
     if (argc < 2) {
-        std::cout << "Please give a file as an argument.\n";
+        std::cout << "\n\n>>> ERROR. Please give a file as an argument.\n";
         return 1;
     }
-    int counter {0};
-    std::string val;
-    std::string arquivo {"../../data/"};
-    arquivo += argv[1];
-    std::ifstream ifs (arquivo); // 2 em 1: Abre o " arquivo . txt " E associa ao fluxo ‘ ifs ’.
-    while ( ifs >> val ) {
-        std::cout << val << "\n";
-        ++counter;
-        if((counter == 1 || counter == 2) && val.empty())
-        {
-            std::cout << "ERROR";
-            break;
+    std::string fileAddress;
+    std::stringstream (argv[1]) >> fileAddress;
+    std::ifstream ifs_bet_file("../../data/" + fileAddress);
 
+    if(!ifs_bet_file.is_open()) 
+    {
+        std::ostringstream oss;
+        oss << "\n\n>>> Sorry, could not open bet file [" << fileAddress <<"].\n    Exiting...\n\n";
+        std::cout << oss.str();
+        return 1;
+    }
+
+    std::string dataLine( "" );
+
+    std::getline (ifs_bet_file, dataLine);
+
+    while(not ifs_bet_file.eof())
+    {
+        auto inputOk{ false };
+
+        if(not inputOk)
+        {
+            
         }
     }
-    ifs.close();
-    std::cout << "Total" << counter << "\n";
 
 
     return EXIT_SUCCESS;
